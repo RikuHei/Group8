99b20fc797d369d54ecc380faf86f965433ef7a5
EetuHei <t8heee01@students.oamk.fi>
2020-11-09 12:55:18 +0200

    basic movement controller, has horizontal movement and jump

---
+++ Dudeman's Adventures/Assets/Scripts/Player-controller/Movement-controller/MovementController.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MovementController : MonoBehaviour
+{
+
+    public CharacterController2D controller;
+    AudioSource audioSource;
+    bool isMoving = false;
+    public float runSpeed = 40f;
+    float horizontalMove = 0f;
+    bool jump = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        // move player character here
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+        jump = false;
+
+    }
+}
