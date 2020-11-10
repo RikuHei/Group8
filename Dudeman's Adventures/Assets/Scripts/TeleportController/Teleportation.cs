13aaaa5956165bdfdc269f88dca5bd6149dd9500
Ossi Karjalainen <t8kaos00@students.oamk.fi>
2020-11-10 19:24:01 +0200

    Added script for teleportation and a test scene for it.

---
+++ Dudeman's Adventures/Assets/Scripts/TeleportController/Teleportation.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleportation : MonoBehaviour
+{
+
+    public GameObject Teleporter, Player;
+    public static int StartTeleport = 0;
+
+
+    public void OnTriggerEnter2D(Collider2D collision) 
+    {
+        if (collision.gameObject.tag == "Player" && Teleportation.StartTeleport == 1)
+        {
+            Teleportation.StartTeleport = 0;
+            StartCoroutine (Teleport());
+        }
+    }
+
+    IEnumerator Teleport()
+    {
+        //Waits for nothing atm but this can be later on changed.
+        yield return new WaitForSeconds(0);
+        //Teleports the player to the position of the set teleporter's vector position.
+        Player.transform.position = new Vector2 (Teleporter.transform.position.x, Teleporter.transform.position.y);
+    }
+}
