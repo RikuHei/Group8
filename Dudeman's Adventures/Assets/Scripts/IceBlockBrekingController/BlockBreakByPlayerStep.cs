f4f084a3869fb8da19232c16e6874324cdcf2470
EetuHei <t8heee01@students.oamk.fi>
2020-11-12 01:18:44 +0200

    destroy blocks when player steps on them

---
+++ Dudeman's Adventures/Assets/Scripts/IceBlockBrekingController/BlockBreakByPlayerStep.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockBreakByPlayerStep : MonoBehaviour
+{
+
+    Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.name.Equals("Player"))
+        {
+            Invoke("BreakPlatform", 0.1f);
+            Destroy(gameObject, 0.1f);
+        }
+    }
+
+    void BreakPlatform()
+    {
+        rb.isKinematic = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
