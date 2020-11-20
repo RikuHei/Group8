ddea12b42ea8220893e68a0ade21e94b055826a0
EetuHei <t8heee01@students.oamk.fi>
2020-11-20 18:09:50 +0200

    script to load next level and increse levelAt state to know what level the player is at

---
+++ Dudeman's Adventures/Assets/Scripts/MoveToNextLevel/MoveToNextLevel.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MoveToNextLevel : MonoBehaviour
+{
+
+    public int nextSceneLoad;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nextSceneLoad = SceneManager.GetActiveScene().buildIndex + 1;
+    }
+
+    public void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.name.Equals("Player"))
+        {
+            // Move to next level
+            SceneManager.LoadScene(nextSceneLoad);
+            Debug.Log(PlayerPrefs.GetInt("levelAt", nextSceneLoad));
+            // Setting Int for Index
+            if (nextSceneLoad > PlayerPrefs.GetInt("levelAt"))
+            {
+                PlayerPrefs.SetInt("levelAt", nextSceneLoad);
+            }
+        }
+    }
+
+}
