b4800a47b5330fa3500167fb0ef898e8183fad78
EetuHei <t8heee01@students.oamk.fi>
2020-11-20 18:10:21 +0200

    level selection to load correct level and lock levels that player should not have access to

---
+++ Dudeman's Adventures/Assets/Scripts/LevelSelection/LevelSelection.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LevelSelection : MonoBehaviour
+{
+
+    public Button[] lvlButtons;
+
+    public void SelectLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int levelAt = PlayerPrefs.GetInt("level", 2);
+        Debug.Log(levelAt);
+        for (int i = 1; i < lvlButtons.Length; i++)
+        {
+            if (1 + 2 > levelAt)
+                lvlButtons[i].interactable = false;
+        }
+    }
+
+}
