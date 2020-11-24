19979ef8bebef81a7b8cc3ac15986b6c7e5e5208
Joel Ala-aho <43337796+Joqz@users.noreply.github.com>
2020-11-24 19:14:23 +0200

    HP & Jump power-ups
    HP power-up works as intented, some pretty annoying bugs currently with the jump power-up: can't get any kinds of timers to work (to reset the jump force back to normal) and the item can sometimes be picked multiple times.
    
    Next I'll try to create a completely new controller that is attached to the player which tracks all of the power-ups.

---
+++ Dudeman's Adventures/Assets/Scripts/Power-upScripts/HPPowerUp.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HPPowerUp : MonoBehaviour
+{
+    //Amount of HP gained when picking the power-up
+    public int HP_amount;
+
+    //Boolean to create power-ups that give maximum HP
+    public bool fullHP;
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if(collider.tag == "Player")
+        {
+            //Getting the script that handles the HP
+            GameObject player = collider.gameObject;
+            RestartOnPlayerDeath playerScript = player.GetComponent<RestartOnPlayerDeath>();
+
+            if(fullHP == true)
+            {
+                playerScript.currentHealth = playerScript.maxHealth;
+            }
+            else
+            {
+                //Checking if the health goes over max health when the power-up is picked up
+                if(playerScript.maxHealth < (playerScript.currentHealth + HP_amount))
+                {
+                    Debug.Log("OVER LIMIT");
+                    playerScript.currentHealth = playerScript.maxHealth;
+                }
+                else
+                {
+                    Debug.Log("UNDER");
+                    playerScript.currentHealth += HP_amount;
+                }
+            }
+
+            playerScript.healthBar.SetHealth(playerScript.currentHealth);
+
+            Destroy(gameObject);
+
+            Debug.Log(playerScript.maxHealth);
+            Debug.Log(playerScript.currentHealth);
+
+        }
+    }
+}
---
+++ Dudeman's Adventures/Assets/Scripts/Power-upScripts/JumpPowerUp.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpPowerUp : MonoBehaviour
+{
+    //How much jump force is added when picking the power-up
+    public float addedForce;
+
+    //How long the power-up lasts when picked up
+    public float powerupTime;
+
+    //Variables for changing the jump force value
+    GameObject player;
+    CharacterController2D playerScript;
+
+    void Awake()
+    {
+        //Getting the script that handles the jump force
+        player = GameObject.FindGameObjectWithTag("Player");
+        playerScript = player.GetComponent<CharacterController2D>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if(collider.tag == "Player")
+        {
+            Destroy(gameObject);
+
+            Debug.Log(playerScript.m_JumpForce);
+
+            StartCoroutine(PowerUpWearOff(5f));
+            //playerScript.m_JumpForce += addedForce;
+            //StartCoroutine(powerUpTimer(powerupTime));
+            //Debug.Log(playerScript.m_JumpForce);
+        }
+    }
+
+    IEnumerator PowerUpWearOff(float time)
+    {
+        Debug.Log("TEST");
+        playerScript.m_JumpForce += addedForce;
+        yield return new WaitForSeconds(time);
+        Debug.Log("TEST1212");
+        playerScript.m_JumpForce -= addedForce;
+
+
+        Debug.Log(playerScript.m_JumpForce);
+    }
+
+/*
+    void CancelPowerUp()
+    {
+        playerScript.m_JumpForce -= addedForce;
+        Debug.Log("Cancelled");
+    }
+*/
+
+
+/*
+    private IEnumerator powerUpTimer(float time)
+    {
+        Debug.Log("Timer starting");
+        Debug.Log(playerScript.m_JumpForce);
+        yield return new WaitForSeconds(time);
+
+        playerScript.m_JumpForce -= addedForce;
+
+        Debug.Log("Timer ending");
+        Debug.Log(playerScript.m_JumpForce);
+    }
+
+
+*/
+    
+
+}
