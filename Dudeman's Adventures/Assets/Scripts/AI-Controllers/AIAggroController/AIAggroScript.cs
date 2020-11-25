907bdca6bad4d8076f38dc03763471e21336c54a
Ossi Karjalainen <t8kaos00@students.oamk.fi>
2020-11-26 00:46:40 +0200

    Doing fine tuning to the linecast detection.

--- Dudeman's Adventures/Assets/Scripts/AI-Controllers/AIAggroController/AIAggroScript.cs
+++ Dudeman's Adventures/Assets/Scripts/AI-Controllers/AIAggroController/AIAggroScript.cs
@@ -13,7 +13,14 @@ public class AIAggroScript : MonoBehaviour
     [SerializeField]
     float moveSpeed;
 
+    [SerializeField]
+    Transform castPoint;
+
     Rigidbody2D rb2d;
+
+    bool isFacingLeft;
+    private bool isAggro = false;
+    private bool isSearching = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -23,7 +30,31 @@ public class AIAggroScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float distToPlayer = Vector2.Distance(transform.position, player.position);
+
+        if(DetectPlayer(aggroRange))
+        {
+            isAggro = true;
+        }
+        else
+        {
+            if(isAggro == true)
+            {
+                if(isSearching == false)
+                {
+                    isSearching = true;
+                    Invoke("StopChasingPlayer", 2);
+                }
+            }
+        }
+
+        if (isAggro == true)
+        {
+            ChasePlayer();
+        }
+        // This code below is for a 'Dumber AI' that will chase the player even if they are
+        // behind a wall. I am just leaving this here because don't know yet how we want to use the aggro mechanic.
+       /* float distToPlayer = Vector2.Distance(transform.position, player.position);
+        transform.Translate(0, 0, 0);
         
         if(distToPlayer < aggroRange)
         {
@@ -32,7 +63,38 @@ public class AIAggroScript : MonoBehaviour
         else
         {
             StopChasingPlayer();
+        }*/
+    }
+
+    bool DetectPlayer(float distance)
+    {
+        bool val = false;
+        float castDist = distance;
+
+        if(isFacingLeft)
+        {
+            castDist = -distance;
         }
+
+        Vector2 endPos = castPoint.position + Vector3.right * castDist;
+
+        RaycastHit2D hit = Physics2D.Linecast(castPoint.position, endPos, 1 << LayerMask.NameToLayer("Player"));
+
+        if(hit.collider != null)
+        {
+            if (hit.collider.gameObject.CompareTag("Player"))
+            {
+                val = true;
+            }
+            else 
+            {
+                val = false;
+            }
+
+        }
+
+        return val;
+
     }
 
     void ChasePlayer()
@@ -42,17 +104,21 @@ public class AIAggroScript : MonoBehaviour
             rb2d.velocity = new Vector2(moveSpeed, 0);
             //these values downhere need to be changed based on the size of the sprite.
             transform.localScale = new Vector2(0.5f, 0.8f);
+            isFacingLeft = false;
         }
         else
         {
             rb2d.velocity = new Vector2(-moveSpeed, 0);
             //these values downhere need to be changed based on the size of the sprite.
             transform.localScale = new Vector2(-0.5f, 0.8f);
+            isFacingLeft = true;
         }
     }
 
     void StopChasingPlayer()
     {
+        isAggro = false;
+        isSearching = false;
         rb2d.velocity = new Vector2(0, 0);
     }
 
@@ -64,5 +130,6 @@ public class AIAggroScript : MonoBehaviour
         }
 
     }
+    
 
 }
