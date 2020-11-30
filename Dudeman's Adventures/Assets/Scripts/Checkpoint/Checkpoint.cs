9b8e2277022334ea9d91ea87ad1c403849b462d7
EetuHei <t8heee01@students.oamk.fi>
2020-11-30 19:26:50 +0200

    check if player collides with checkpoints and update pos for gamemaster

---
+++ Dudeman's Adventures/Assets/Scripts/Checkpoint/Checkpoint.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour
+{
+
+    private GameMaster gm;
+    void Start()
+    {
+        gm = GameObject.FindGameObjectWithTag("GM").GetComponent<GameMaster>();
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            gm.lastCheckpointPos = transform.position;
+        }
+    }
+
+}
