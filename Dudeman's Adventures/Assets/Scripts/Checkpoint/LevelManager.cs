eb0421598d7ed6823af3f79c9b7e2d43c1617d15
EetuHei <t8heee01@students.oamk.fi>
2020-12-02 23:52:09 +0200

    respawn function for player that takes pos based on active checkpoint

---
+++ Dudeman's Adventures/Assets/Scripts/Checkpoint/LevelManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+
+    public GameObject currentCheckpoint;
+
+    private CharacterController2D player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = FindObjectOfType<CharacterController2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void RespawnPlayer()
+    {
+        Debug.Log("Player respawned");
+        player.transform.position = currentCheckpoint.transform.position;
+    }
+
+}
