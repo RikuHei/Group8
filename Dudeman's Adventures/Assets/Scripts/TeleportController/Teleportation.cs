e3693ec69d3076715416c22d4d39df0b9419c5c3
Ossi Karjalainen <t8kaos00@students.oamk.fi>
2020-12-08 13:44:56 +0200

    Added teleportation sounds and adjusted layers accordingly.

--- Dudeman's Adventures/Assets/Scripts/TeleportController/Teleportation.cs
+++ Dudeman's Adventures/Assets/Scripts/TeleportController/Teleportation.cs
@@ -7,21 +7,29 @@ public class Teleportation : MonoBehaviour
 
     public GameObject TeleportToLocation, Player;
     public static int StartTeleport = 0;
+    public AudioSource audioSource;
+    public AudioListener audioListener;
+    public AudioClip soundClip;
 
-
+    void Start()
+    {
+        audioListener = GetComponent<AudioListener>();
+        audioSource = gameObject.GetComponent<AudioSource>();
+    }
     public void OnTriggerEnter2D(Collider2D collision) 
     {
         //Checks if the object is tagged Player and if the player has passed key positions.
-        if (collision.gameObject.tag == "Player" && Teleportation.StartTeleport == 1)
+        if (collision.gameObject.name == "Player" && Teleportation.StartTeleport == 1)
         {
             Teleportation.StartTeleport = 0;
-            Invoke("Teleport", 0.3f);
+            Invoke("Teleport", 0.2f);
         }
     }
 
     void Teleport()
     {
-
+        audioSource.clip = soundClip;
+        audioSource.Play();
         //Teleports the player to the position of the set teleporter's vector position.
         Player.transform.position = new Vector3 (TeleportToLocation.transform.position.x, TeleportToLocation.transform.position.y, -10);
     }
