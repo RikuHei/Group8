6829ce9a9156e90e64c65781f2c38dda89ced6d2
Joel Ala-aho <43337796+Joqz@users.noreply.github.com>
2020-12-10 17:35:44 +0200

    Added ability to flip door controls, pressureplates can be activated by player etc

--- Dudeman's Adventures/Assets/Scripts/InteractableObjectScripts/PressurePlateController.cs
+++ Dudeman's Adventures/Assets/Scripts/InteractableObjectScripts/PressurePlateController.cs
@@ -4,24 +4,47 @@ using UnityEngine;
 
 public class PressurePlateController : MonoBehaviour
 {
-
     public bool pressurePlateIsActivated = false; 
 
-    //When something hits the hitbox of the pressure plate, change the boolean to true
-    void OnCollisionEnter2D(Collision2D other)
+    public bool pressurePlateTimer = false;
+    public float timeUntilReset;
+    
+
+    SpriteRenderer spriteRenderer;
+
+    void Start()
     {
-        if (other.transform.tag == "Moveable") 
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    //Pressure plate is active when the object stays on it
+    void OnCollisionStay2D(Collision2D other)
+    {
+        if (other.transform.tag == "Moveable" || other.transform.tag == "Player") 
         {
             pressurePlateIsActivated = true;
+            spriteRenderer.color = Color.green;
         }
     }
 
     //Vice versa
     void OnCollisionExit2D(Collision2D other)
     {
-        if (other.transform.tag == "Moveable") 
+        if (other.transform.tag == "Moveable" || other.transform.tag == "Player" && !pressurePlateTimer) 
         {
             pressurePlateIsActivated = false;
+            spriteRenderer.color = Color.red;
         }
+        if (other.transform.tag == "Player" && pressurePlateTimer) 
+        {
+            StartCoroutine(ResetTimer(timeUntilReset));
+        }
+    }
+
+    public IEnumerator ResetTimer(float time)
+    {
+        yield return new WaitForSeconds(time);
+        pressurePlateIsActivated = false;
+        spriteRenderer.color = Color.red;
     }
 }
