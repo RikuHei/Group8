3d55f7708b71be01984a181078b35ab37ff12fe4
EetuHei <t8heee01@students.oamk.fi>
2020-12-11 16:20:07 +0200

    more functionality for state machine behaviour, boss runs at player when player is too far from attack range and when in range boss tries to attack. This will need a a lot of fine tuning to work with shooting

--- Dudeman's Adventures/Assets/Scripts/BossBattle/BossRun.cs
+++ Dudeman's Adventures/Assets/Scripts/BossBattle/BossRun.cs
@@ -7,8 +7,10 @@ public class BossRun : StateMachineBehaviour
 
     Transform player;
     Rigidbody2D rb;
+    Boss boss;
 
-    public float speed = 10f;
+    public float speed = 8f;
+    public float attackRange = 3f;
 
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
@@ -18,19 +20,32 @@ public class BossRun : StateMachineBehaviour
 
         // get boss Rigidbody2D
         rb = animator.GetComponent<Rigidbody2D>();
+
+        // get boss Object
+        boss = animator.GetComponent<Boss>();
     }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
+        // call for function to flip the boss so he is always facing the way where the player is
+        boss.LookAtPlayer();
+
+        // get target and move boss to target
         Vector2 target = new Vector2(player.position.x, rb.position.y);
         Vector2 newPos = Vector2.MoveTowards(rb.position, target, speed * Time.fixedDeltaTime);
         rb.MovePosition(newPos);
+
+        // if boss is cloes engouh to player, he will attack
+        if (Vector2.Distance(player.position, rb.position) <= attackRange)
+        {
+            animator.SetTrigger("Attack");
+        }
     }
 
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    {
-
+    {   // reset animator Attack to prevent weired behaviour
+        animator.ResetTrigger("Attack");
     }
 }
