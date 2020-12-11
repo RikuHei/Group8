b0fe123f91c85e0bb6836c736f6b3648d3135680
EetuHei <t8heee01@students.oamk.fi>
2020-12-11 19:09:17 +0200

    added bosshp script into bullet script

--- Dudeman's Adventures/Assets/Scripts/BossBattle/BossWeapon.cs
+++ Dudeman's Adventures/Assets/Scripts/BossBattle/BossWeapon.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class BossWeapon : MonoBehaviour
 {
     public int attackDamage = 20;
-    public int enrageAttackDamage = 40;
+    public int enragedAttackDamage = 40;
 
     public Vector3 attackOffset;
     public float attackRange = 3f;
@@ -23,5 +23,10 @@ public class BossWeapon : MonoBehaviour
         {
             colInfo.GetComponent<RestartOnPlayerDeath>().TakeDamage(attackDamage);
         }
+    }
+
+    public void EnragedAttack()
+    {
+
     }
 }
