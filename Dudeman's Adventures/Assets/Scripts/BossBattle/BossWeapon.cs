574791d51ffbd56d793ed51df940b906bf21e15f
EetuHei <t8heee01@students.oamk.fi>
2020-12-14 14:49:13 +0200

    added enraged attack range to be 1, and enraged attack deal 200 damage, which is one shot.

--- Dudeman's Adventures/Assets/Scripts/BossBattle/BossWeapon.cs
+++ Dudeman's Adventures/Assets/Scripts/BossBattle/BossWeapon.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class BossWeapon : MonoBehaviour
 {
     public int attackDamage = 20;
-    public int enragedAttackDamage = 40;
+    public int enragedAttackDamage = 200;
 
     public Vector3 attackOffset;
     public float attackRange = 3f;
+    private float enrageAttackRange = 1f;
     public LayerMask attackMask;
 
     public void Attack()
@@ -27,6 +28,13 @@ public class BossWeapon : MonoBehaviour
 
     public void EnragedAttack()
     {
+        // collision based damage taken
+        Vector3 pos = transform.position;
 
+        Collider2D colInfo = Physics2D.OverlapCircle(pos, enrageAttackRange, attackMask);
+        if (colInfo != null)
+        {
+            colInfo.GetComponent<RestartOnPlayerDeath>().TakeDamage(enragedAttackDamage);
+        }
     }
 }
