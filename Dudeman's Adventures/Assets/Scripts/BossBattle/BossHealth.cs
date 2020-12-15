aaa74e9c1e49a70412c35e2a5314ad9a99d3d9e2
EetuHei <t8heee01@students.oamk.fi>
2020-12-15 15:48:16 +0200

    when boss dies, move to next level door activates

--- Dudeman's Adventures/Assets/Scripts/BossBattle/BossHealth.cs
+++ Dudeman's Adventures/Assets/Scripts/BossBattle/BossHealth.cs
@@ -9,6 +9,10 @@ public class BossHealth : MonoBehaviour
 
     public bool isInvulnerable = false;
 
+    private GameObject nextLevel;
+
+    private bool bossDead = false;
+
     public void TakeDamage(int damage)
     {
         if (isInvulnerable)
@@ -32,17 +36,27 @@ public class BossHealth : MonoBehaviour
     {
         // play death animation.
         GetComponent<Animator>().SetBool("IsDead", true);
+        bossDead = true;
     }
 
     // Start is called before the first frame update
     void Start()
     {
-
+        nextLevel = GameObject.Find("NextLevel");
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if (!bossDead)
+        {
+            Debug.Log("door should not be active now");
+            nextLevel.GetComponent<Collider2D>().enabled = false;
+        }
+        else
+        {
+            Debug.Log("door should be active now");
+            nextLevel.GetComponent<Collider2D>().enabled = true;
+        }
     }
 }
